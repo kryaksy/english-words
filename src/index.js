19af76e8aa06d8e19e1d5388d43e666b08cdd16b
Ibrahim Aksoy <ib.koray@gmail.com>
2020-08-13 20:41:04 +0300

    Random word with meaning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const words = require("./words.json");
+const dictionary = require("./dictionary.json");
 const uniqueRandomArray = require("unique-random-array");
+
+const getWord = uniqueRandomArray(words);
 
 module.exports = {
   all: words,
-  random: uniqueRandomArray(words),
+  random: getWord,
+  randomMeaning: () => {
+    const word = getWord();
+    return [word, dictionary[getWord()]];
+  },
 };
