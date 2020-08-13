6af3ce53dd5e876c80789dcbee0d8953a1a50122
Ibrahim Aksoy <ib.koray@gmail.com>
2020-08-13 20:03:29 +0300

    removed lowercase

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const uniqueRandomArray = require("unique-random-array");
 
 module.exports = {
   all: words,
-  random: uniqueRandomArray(words).toLoverCase(),
+  random: uniqueRandomArray(words),
 };
